0a28fddea71927f39ac1b8a17c363e77f1ad1a76
Vik Paruchuri <vik.paruchuri@gmail.com>
2013-07-02 19:37:20 -0400

    Produce complete coverage report

--- percept-proto/tests/commands/test.py
+++ percept-proto/tests/commands/test.py
@@ -5,12 +5,24 @@ Command line command to run tests
 import nose
 from management.commands import BaseCommand
 from conf.base import settings
+import pkgutil
+from importlib import import_module
+import os
 
 class Command(BaseCommand):
     def command(self, *args, **options):
         #Grab the pythonpath argument and look for tests there
         app_names = settings.INSTALLED_APPS
-        app_labels = list(set(["--cover-package={0}".format(a.split('.')[0]) for a in app_names]))
+        #Grab the top level app name from installed_apps
+        app_labels = list(set([a.split('.')[0] for a in app_names]))
+        app_paths = []
+        #We want to figure out coverage for the "lower-level" apps, so import all the top level apps
+        #and get their paths
+        for al in app_labels:
+            mod = import_module(al)
+            app_paths.append(os.path.dirname(mod.__file__))
+        #Pass paths to pkgutil to get the names of the submodules
+        sub_labels = [name for _, name, _ in pkgutil.iter_modules(app_paths)]
         #Produce a coverage report for installed_apps
-        argv = ['{0}'.format(options['pythonpath']), '--with-coverage', ' '.join(app_labels)]
+        argv = ['{0}'.format(options['pythonpath']), '--with-coverage', '--cover-package={0}'.format(','.join(app_labels + sub_labels))]
         nose.run(argv=argv)
